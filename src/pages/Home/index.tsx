e77edee0a3b4f541dc07df69578d52b8c6e919a5
Aline Prasser <aline.prasser@codenapp.com>
2023-04-03 21:37:44 -0300

    Aba esquerda

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,23 @@ export function Home() {
             <div className="box">
               <div className="box-header"></div>
               <div className="box-content">
-                <div className="left-box-content">left</div>
+                <div className="left-box-content">
+                  <img
+                    className="profile-img"
+                    src="src\assets\images\profile.jpg"
+                  />
+                  <div className="left-box-content-text">
+                    <h1>
+                      Olá, sou a Aline!<span>ela/dela</span>
+                    </h1>
+                    <p>
+                      Desenvolvedora <strong>Front-End</strong> e{" "}
+                      <strong>UX/UI</strong> Designer há cerca de três anos e
+                      encantada por realizar projetos que impactam positivamente
+                      na vida das pessoas! ✨💖
+                    </p>
+                  </div>
+                </div>
                 <div className="center-box-content">center</div>
                 <div className="right-box-content">right</div>
               </div>
